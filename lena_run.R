04ce89660b855d1ba88d865d5b7b26fed6832087
awp-finanznachrichten <sw@awp.ch>
2023-06-05 10:49:53 +0200

    commit from Rstudio

--- lena_run.R
+++ lena_run.R
@@ -1,7 +1,7 @@
 #repeat{
 
 #Working Directory definieren
-setwd("C:/Users/simon/OneDrive/LENA_Project/20230618_LENA_Abstimmungen")
+setwd("C:/Users/sw/OneDrive/LENA_Project/20230618_LENA_Abstimmungen")
 
 ###Config: Bibliotheken laden, Pfade/Links definieren, bereits vorhandene Daten laden
 source("config.R",encoding = "UTF-8")
@@ -56,7 +56,7 @@ source("kantonale_abstimmungen_uebersicht.R", encoding="UTF-8")
 
 #Make Commit
 git2r::config(user.name = "awp-finanznachrichten",user.email = "sw@awp.ch")
-token <- read.csv("C:/Users/simon/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
+token <- read.csv("C:/Users/sw/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
 git2r::cred_token(token)
 gitadd()
 gitcommit()
